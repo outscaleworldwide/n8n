844de7821bfc7dcdf085ff7f4ef7fe0b01bf4a8d
outscaleworldwide <outscaleworldwide@gmail.com>
2025-04-08 15:46:44 +0530

    Update docker-entrypoint.sh

--- docker/images/n8n/docker-entrypoint.sh
+++ docker/images/n8n/docker-entrypoint.sh
@@ -1,15 +1,29 @@
-#!/bin/sh
-if [ -d /opt/custom-certificates ]; then
-  echo "Trusting custom certificates from /opt/custom-certificates."
-  export NODE_OPTIONS="--use-openssl-ca $NODE_OPTIONS"
-  export SSL_CERT_DIR=/opt/custom-certificates
-  c_rehash /opt/custom-certificates
-fi
+# Setup the Task Runner Launcher
+ARG TARGETPLATFORM
+ARG LAUNCHER_VERSION=1.1.1
+COPY n8n-task-runners.json /etc/n8n-task-runners.json
 
-if [ "$#" -gt 0 ]; then
-  # Got started with arguments
-  exec n8n "$@"
-else
-  # Got started without arguments
-  exec n8n
-fi
+# Download, verify, and extract the launcher binary
+RUN \
+	if [[ "$TARGETPLATFORM" = "linux/amd64" ]]; then export ARCH_NAME="amd64"; \
+	elif [[ "$TARGETPLATFORM" = "linux/arm64" ]]; then export ARCH_NAME="arm64"; fi; \
+	mkdir /launcher-temp && \
+	cd /launcher-temp && \
+	wget https://github.com/n8n-io/task-runner-launcher/releases/download/${LAUNCHER_VERSION}/task-runner-launcher-${LAUNCHER_VERSION}-linux-${ARCH_NAME}.tar.gz && \
+	wget https://github.com/n8n-io/task-runner-launcher/releases/download/${LAUNCHER_VERSION}/task-runner-launcher-${LAUNCHER_VERSION}-linux-${ARCH_NAME}.tar.gz.sha256 && \
+	echo "$(cat task-runner-launcher-${LAUNCHER_VERSION}-linux-${ARCH_NAME}.tar.gz.sha256) task-runner-launcher-${LAUNCHER_VERSION}-linux-${ARCH_NAME}.tar.gz" > checksum.sha256 && \
+	sha256sum -c checksum.sha256 && \
+	tar xvf task-runner-launcher-${LAUNCHER_VERSION}-linux-${ARCH_NAME}.tar.gz --directory=/usr/local/bin && \
+	cd - && \
+	rm -r /launcher-temp
+
+# ✅ Add the entrypoint script and make it executable
+COPY docker-entrypoint.sh /
+RUN chmod +x /docker-entrypoint.sh
+
+RUN \
+	mkdir .n8n && \
+	chown node:node .n8n
+ENV SHELL /bin/sh
+USER node
+ENTRYPOINT ["tini", "--", "/docker-entrypoint.sh"]
